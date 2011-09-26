b08c530f9ff85b8feb82103fd62ca7b779de5a63
Kim Ki-yong <unsouled0@gmail.com>
2011-09-26 19:29:36 +0900

    add hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  printf("Hello, world\n");
+  return 0;
+}
